444e49d33ba4a9fba4e4260e6adb4e41a921348e
Ênio Ferreira <79934130+enioo22@users.noreply.github.com>
2024-11-07 16:47:57 +0000

    version 1.1.3

--- custom_components/bzutech/binary_sensor.py
+++ custom_components/bzutech/binary_sensor.py
@@ -1,6 +1,7 @@
 """Binary sensor for BzuTech Integration."""
 
 from datetime import timedelta
+import json
 import logging
 from typing import Any
 
@@ -11,13 +12,16 @@ from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
     BinarySensorEntity,
 )
+from homeassistant.components.logbook import log_entry, async_log_entry
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import dt as dt_util
+from homeassistant.util.json import JsonObjectType
 
-from .config_flow import get_entities, get_sensortype
+from .config_flow import get_all_entities, get_entities, get_sensortype
 from .const import CONF_CHIPID, CONF_ENTITY, CONF_SENSORNAME, DOMAIN
 
 SCAN_INTERVAL = timedelta(seconds=30)
@@ -61,11 +65,44 @@ async def async_setup_entry(
     async_add_entities(sensors, update_before_add=True)
 
 
+def get_conditions(event: JsonObjectType):
+    c = "  condition:\n"
+    if "condition" not in event:
+        return "  - condition: []\n"
+    if len(event["condition"]) == 0:
+        return "  - condition: []\n"
+
+    for e in event["condition"]:
+        c = c + f"  - condition: {e["condition"]}\n"
+        c = c + f"    entity_id: {e["entity_id"]}\n"
+        if "above" in e:
+            c = c + f"    above: {e["above"]}\n"
+        if "below" in e:
+            c = c + f"    below: {e["below"]}\n"
+
+    return c
+
+
+def get_actions(event: JsonObjectType):
+    a = "  action:\n"
+    if "action" in event:
+        for e in event["action"]:
+            a = a + "  - action: mqtt.publish\n"
+            a = a + r"    metadata: {}"
+            a = a + "    data:\n"
+            a = a + "      qos: 1\n"
+            a = a + "      topic: haalertreturn/"
+            a = a + f"      title: {e["data"]["title"]}\n"
+
+    return a
+
+
 class BzuBinarySensorEntity(BinarySensorEntity):
     """Bzutech binary sensor entity."""
 
     sent_updatechannels = False
     number_entities = 0
+    entity_id_bzu = {}
 
     def __init__(self, api, entry: ConfigEntry) -> None:
         """Set up binary sensor."""
@@ -97,14 +134,33 @@ class BzuBinarySensorEntity(BinarySensorEntity):
             sw_version="1.0",
         )
 
-    async def async_update(self) -> None:
+    def get_triggers(self, event: JsonObjectType):
+        t = "  trigger:\n"
+        print(self.entity_id_bzu)
+        event["entity_id"] = self.entity_id_bzu[event["canal_id"]]
+        t = (
+            t
+            + f"  - platform: numeric_state\n    entity_id:\n    - {event["entity_id"]}\n"
+        )
+        if event["alerta_operador"] == ">":
+            t = t + f"    above: {event["alerta_valor"]}\n"
+        if event["alerta_operador"] == "<":
+            t = t + f"    below: {event["alerta_valor"]}\n"
+        if event["alerta_operador"] == "=":
+            t = t + f"    above: {event["alerta_valor"]}\n"
+            t = t + f"    below: {int(event["alerta_valor"])+1}\n"
+
+        return t
+
+    async def async_update(
+        self,
+    ) -> None:
         """Upload Readings to cloud."""
 
         date = str(dt_util.as_local(dt_util.now()))[:19]
-        counter = 1
         entidades = []
         if self.sendall == 1:
-            entidades = get_entities(self.hass)[1:]
+            entidades = get_all_entities(self.hass)[1:]
 
         readings: dict[str, Any] = {}
         readings["Records"] = []
@@ -118,7 +174,87 @@ class BzuBinarySensorEntity(BinarySensorEntity):
         channels["Records"][0]["bci"] = self.chipid
         chs = []
 
+        @callback
+        async def async_call_service_mqtt(msg: mqtt.ReceiveMessage) -> None:
+            call = json.loads(msg.payload)
+
+            entity = call["entity"]
+            funcao = call["method"]
+            entitytype = entity.split(".")[0]
+            retorno = {}
+            retorno["info"] = "funcao invalida"
+            if funcao in self.hass.services.async_services_for_domain(entitytype):
+                await self.hass.services.async_call(
+                    entitytype, funcao, {ATTR_ENTITY_ID: [entity]}, True
+                )
+                retorno["info"] = "sucesso"
+            mqtt.publish(self.hass, "hacallreturn", str(retorno))
+
+        @callback
+        async def async_create_automation(msg: mqtt.ReceiveMessage) -> None:
+            automation = "\n"
+            event = json.loads(msg.payload)
+            print(msg.payload)
+
+            necessary_keys = [
+                "alerta_id",
+                "alerta_valor",
+                "alerta_operador",
+                "canal_id",
+            ]
+
+            if all(k in event for k in necessary_keys):
+                automation = automation + f"- id: '{event["alerta_id"]}'\n"
+                automation = automation + "  alias: 'Alarme Bzu Cloud'\n"
+                automation = (
+                    automation
+                    + f"  description: 'Alarme Bzu Cloud #{event["alerta_id"]}'\n"
+                )
+                automation = automation + "  mode: single\n"
+                automation = automation + self.get_triggers(event)
+                # automation = automation + get_conditions(event)
+                automation = automation + "  action:\n"
+                automation = automation + "  - action: mqtt.publish\n"
+                automation = automation + "    metadata: {}\n"
+                automation = automation + "    data:\n"
+                automation = automation + "      qos: 1\n"
+                automation = (
+                    automation
+                    + f"      topic: ha_alert_action/{self.chipid.split("-")[1]}\n"
+                )
+
+                payload = (
+                    '\'{"Records": [{"alerta_id":'
+                    + str(event["alerta_id"])
+                    + ', "value": {{states("'
+                    + self.entity_id_bzu[event["canal_id"]]
+                    + "\")}} }]}'"
+                )
+                automation = automation + f"      payload: {payload}"
+
+                with open(r"config/automations.yaml", mode="r+", encoding="utf-8") as f:
+                    size = len(f.read())
+                    f.close()
+                # print(automation)
+                with open(r"config/automations.yaml", "a+", encoding="utf-8") as f:
+                    if size < 5:
+                        f.truncate(0)
+                    f.write(automation)
+                    f.close()
+                await self.hass.services.async_call("automation", "reload")
+
         if await mqtt.async_wait_for_mqtt_client(self.hass):
+            await mqtt.async_subscribe(
+                self.hass,
+                f"hacall/{self.chipid.split("-")[1]}",
+                async_call_service_mqtt,
+            )
+            await mqtt.async_subscribe(
+                self.hass,
+                f"alerta_ha/{self.chipid.split("-")[1]}",
+                async_create_automation,
+                2,
+            )
             for entity in entidades:
                 try:
                     stt = self.hass.states.get(entity)
@@ -128,14 +264,22 @@ class BzuBinarySensorEntity(BinarySensorEntity):
                     logging.error("Sensor name error")
                     return
                 sensor = f"HA-{get_sensortype(self.hass, entity)}-{stringtoint(entity)}"
+                self.entity_id_bzu[sensor] = entity
                 chs.append(sensor)
-                counter = counter + 1
                 data.append({"ref": sensor, "med": reading})
             channels["Records"][0]["channels"] = str(chs).replace("'", r'*"')
             readings["Records"][0]["data"] = str(data).replace("'", r'*"')
             if len(entidades) != self.number_entities:
                 self.number_entities = len(entidades)
+                for key in self.entity_id_bzu:  # pylint: disable=consider-using-dict-items
+                    log_entry(
+                        self.hass,
+                        "Sensor Name Bzu Cloud",
+                        f"{self.entity_id_bzu[key]} -> {key}",
+                        DOMAIN,
+                        "binary_sensor.bzu_cloud",
+                    )
                 mqtt.publish(self.hass, "UpdateChannels", str(channels))
+            logging.warning(self.entity_id_bzu)
             mqtt.publish(self.hass, "data_send", str(readings))
-
         self._attr_is_on = True
--- custom_components/bzutech/config_flow.py
+++ custom_components/bzutech/config_flow.py
@@ -21,12 +21,13 @@ from homeassistant.helpers.selector import (
     SelectSelectorConfig,
     SelectSelectorMode,
 )
+from homeassistant.helpers.selector import BooleanSelector, BooleanSelectorConfig
+from homeassistant.components.input_boolean import InputBoolean
 
 from .const import (
     CONF_CHIPID,
     CONF_ENDPOINT,
     CONF_ENTITY,
-    CONF_SENDALL,
     CONF_SENSORNAME,
     CONF_SENSORPORT,
     CONF_TYPE,
@@ -47,6 +48,9 @@ sensortypes = {
     "pm25": "P25",
     "signal_strength": "DBM",
     "aqi": "DOR",
+    "switch": "SWT",
+    "light": "LIT",
+    "remote": "RMT",
 }
 
 
@@ -63,7 +67,8 @@ STEP_USER_LOGIN_SCHEMA = vol.Schema(
 
 async def get_api(hass: HomeAssistant, data: dict[str, Any]) -> BzuTech:
     """Validate the user input allows us to connect."""
-    return BzuTech(data[CONF_EMAIL], data[CONF_PASSWORD])
+    api = BzuTech(data[CONF_EMAIL], data[CONF_PASSWORD])
+    return api
 
 
 def get_ports(api: BzuTech, chipid: str) -> list[str]:
@@ -71,31 +76,39 @@ def get_ports(api: BzuTech, chipid: str) -> list[str]:
     return [f"Port {i} {api.get_endpoint_on(chipid, i)}" for i in range(1, 5)]
 
 
-def get_entities(hass: HomeAssistant) -> list[str]:
+def get_all_entities(hass: HomeAssistant) -> dict[str, str]:
+    entitylist = get_entities(hass)[1:]
+    for x in hass.states.async_entity_ids(["switch", "light", "remote"]):
+        entitylist.append(x)
+    return entitylist
+
+
+def get_entities(hass: HomeAssistant) -> dict[str, str]:
     """Get every entity name to send them to bzucloud."""
+    entityList = []
+    for x in hass.states.async_entity_ids(["sensor"]):
+        try:
+            attr = hass.states.get(x).as_dict()["attributes"]
+            if "device_class" in attr:
+                if attr["device_class"] != "timestamp":
+                    entityList.append(x)
+        except:
+            continue
 
-    entityList = ["Add all"]
-    for entity in hass.states.async_entity_ids(["sensor"]):
-        stt = hass.states.get(entity)
-        if stt is not None:
-            dv = str(stt.as_dict()["attributes"])
-            dv = dv[dv.index("'device_class': '") : dv.index("', 'friendly_name'")]
-            dv = dv[17:]
-            if dv != "timestamp":
-                entityList.append(entity)
-
+    entityList.insert(0, "Add all")
     return entityList
 
 
 def get_sensortype(hass: HomeAssistant, entity: str):
     """Get the sensor type to be send."""
-    ent = hass.states.get(entity)
-    if ent is not None:
-        dv = str(ent.as_dict()["attributes"])
-        dv = dv[dv.index("'device_class': '") : dv.index("', 'friendly_name'")]
-        dv = dv[17:]
-        return sensortypes[dv]
-    return ""
+    if entity.split(".")[0] == "sensor":
+        try:
+            return sensortypes[
+                hass.states.get(entity).as_dict()["attributes"]["device_class"]
+            ]
+        except:
+            return "GEN"
+    return sensortypes[entity.split(".")[0]]
 
 
 def get_sensornumber(hass: HomeAssistant):
@@ -112,7 +125,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     password = ""
     actual = 0
     selecteddevice = 0
-    selectedtype = ""
+    selectedtype = 0
     selectedentity = ""
     selectedport = 0
 
@@ -125,7 +138,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             try:
                 self.api = await get_api(self.hass, user_input)
                 if not await self.api.start():
-                    raise InvalidAuth("Authentication Error")
+                    raise InvalidAuth("Invalid Auth")
             except InvalidAuth:
                 _LOGGER.exception("Invalid Auth")
                 errors["base"] = "Invalid Auth"
@@ -133,7 +146,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             self.email = user_input[CONF_EMAIL]
             self.password = user_input[CONF_PASSWORD]
-            if not self.hass.states.get("binary_sensor.ha_sendall"):
+            if not self.hass.states.get("binary_sensor.bzu_cloud"):
                 return await self.async_step_typeselect(user_input=user_input)
             return await self.async_step_deviceselect(user_input)
         return self.async_show_form(
@@ -144,26 +157,26 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
     async def async_step_addentities(
-        self, user_input: dict[str, Any]
+        self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Select entities to be send."""
         if CONF_ENTITY in user_input:
-            self.selectedentity = user_input[CONF_ENTITY]
             user_input = {
                 CONF_PASSWORD: self.password,
                 CONF_TYPE: self.selectedtype,
                 CONF_EMAIL: self.email,
-                CONF_ENTITY: self.selectedentity,
-                CONF_SENDALL: 0,
+                CONF_ENTITY: user_input[CONF_ENTITY],
+                "todos": 0,
                 CONF_CHIPID: f"HA-{re.sub("[a-zA-z]", "", self.hass.data["core.uuid"][-7:])}",
-                CONF_SENSORNAME: f"HA-{get_sensortype(self.hass, user_input[CONF_ENTITY])}-{get_sensornumber(self.hass)}",
+                CONF_SENSORNAME: f"HA-GEN-{get_sensornumber(self.hass)}",
             }
             if (
                 user_input[CONF_ENTITY] == "Add all"
                 or user_input[CONF_ENTITY] == "Add every device"
             ):
-                user_input[CONF_SENDALL] = 1
-                user_input[CONF_SENSORNAME] = "HA-SENDALL"
+                # user_input[CONF_ENTITY] = get_entities(self.hass)
+                user_input["todos"] = 1
+                user_input[CONF_SENSORNAME] = "Bzu Cloud"
             return self.async_create_entry(
                 title=user_input[CONF_CHIPID],
                 data=user_input,
@@ -189,7 +202,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         )
 
     async def async_step_typeselect(
-        self, user_input: dict[str, Any]
+        self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         "Select if data will be gotten or sent."
         if CONF_TYPE in user_input:
